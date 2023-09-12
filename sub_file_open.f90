d8184ab27c15e7a386ce6cf1e393907d26b7c221
takumi_akahoshi <takumi.0205.akapon@gmail.com>
2023-09-12 10:18:37 +0900

    初期計算のみです

---
+++ sub_file_open.f90
@@ -0,0 +1,12 @@
+subroutine file_open
+    open(1,file='chemical_input.txt')
+    
+    open(10,file='output.txt')
+    open(11,file='initial.txt')
+    open(12,file='aaa.txt')
+    open(13,file='iii.txt')
+    open(14,file='iniconditon.txt')
+
+    
+    end subroutine
+    
